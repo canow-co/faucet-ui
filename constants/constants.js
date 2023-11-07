6a61d23daf807c5d836a470d588b2639168c0b2d
ghabxph <11975597+ghabxph@users.noreply.github.com>
2023-11-07 13:39:51 +0800

    feat(#55): changed denom to zarx

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,4 @@
 export const CHEQD_FAUCET_SERVER = 'https://faucet.testnet.canowchain.com/backend' 
-export const CHEQD_MINIMAL_DENOM = process.env.CF_CHEQD_MINIMAL_DENOM || 'ncheq'
+export const CHEQD_MINIMAL_DENOM = process.env.CF_CHEQD_MINIMAL_DENOM || 'zarx'
 export const CHEQD_CURRENT_AMOUNT_GIVEN = process.env.CF_CHEQD_CURRENT_AMOUNT_GIVEN || 5000000000000000
 export const DEFAULT_TESTING_ADDRESS = process.env.CF_DEFAULT_TESTING_ADDRESS || 'canow1pzxt656sxd65dsnkpqyyj5y2dm5vw7gc5vhhv6'
